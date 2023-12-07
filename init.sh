b7e6b8925d328541d408535a9f8f566d73578f8a
Eli Sauvage <36312455+eli-sauvage@users.noreply.github.com>
2023-12-07 19:30:29 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,5 +6,9 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
+# rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rust.sh
+chmod +x rust.sh
+./rust.sh -y
 # astronvim
 git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
