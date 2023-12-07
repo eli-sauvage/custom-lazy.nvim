b645f0b3288decd17e90e2e02c862dc7de901155
Eli Sauvage <36312455+eli-sauvage@users.noreply.github.com>
2023-12-07 19:28:22 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #deps
 sudo apt update
-sudo apt install -y git curl libfuse-dev fuse gcc g++ make ripgrep unzip
+sudo apt install -y git curl libfuse-dev fuse gcc g++ make ripgrep unzip trash-cli
 #nvim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
-#lazy
-mkdir -p ~/.config/nvim
-cp -r ./config/* ~/.config/nvim/
+curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
+# astronvim
+git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
