b6e26a7611867d8928f14d74894d50f56889c1c4
Eli Sauvage <36312455+eli-sauvage@users.noreply.github.com>
2024-01-21 22:57:14 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #deps
 sudo apt update
-sudo apt install -y git curl libfuse-dev fuse gcc g++ make unzip trash-cli ripgrep
+sudo apt install -y git curl libfuse-dev fuse gcc g++ make unzip trash-cli ripgrep tmux
 #nvim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
@@ -11,12 +11,10 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > rust.sh
 chmod +x rust.sh
 ./rust.sh -y
 source "$HOME/.cargo/env"
-# zellij
-wget https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz
-tar -xvf zellij*.tar.gz
-chmod +x zellij
-sudo mv zellij /usr/local/bin/zellij
-mkdir ~/.config/zellij
-echo pane_frames false > ~/.config/zellij/config.kdl
+# ohmytmux
+cd
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
 # astronvim
 git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
